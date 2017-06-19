9db4088f6e86a52038668a38aa5dcddc614ba0d3
Wilson Berkow <wilsonberkow@gmail.com>
2017-06-19 00:21:51 -0400

    Flatten main function (by restructuring match blocks)

--- src/main.rs
+++ src/main.rs
@@ -32,72 +32,73 @@ use std::time::Instant;
 use consts::*;
 
 fn main() {
-    // TODO: flatten structure of `match`es here
+    // Read 'script' file to String
+    let mut s = "".to_owned();
     match File::open("script") {
         Err(e) => {
-            panic!("Could not open file 'script'. Error: {}", e);
+            println!("Error opening file 'script': {}", e);
+            return;
         },
-        Ok(mut file) => {
-            let mut s = String::from("");
-            match file.read_to_string(&mut s) {
-                Ok(_) => {
-                    // `tx` is sent to run_script, which generates frames (on the heap) and
-                    // sends them to `tx`. `rx` is given to spawn_saver which duplicates it
-                    // into several worker threads for saving frames to a file.
-                    let (tx, rx) = channel();
-                    let handle = ppm::spawn_saver(rx);
-                    println!("Using {} worker threads to save and convert frames", NUM_WORKERS);
-
-                    // Make `anim` directory unless it exists
-                    ppm::mkdirp("anim");
-
-                    // Generate frames:
-                    let start = Instant::now();
-
-                    // Parse script and exit on syntax error
-                    let cmds = match parse::parse_script(&s) {
-                        Ok(cmds) => cmds,
-                        Err(parse_error) => {
-                            println!("\n{}", parse_error);
-                            return;
-                        }
-                    };
-
-                    // frame_info, if Some, is a pair of the number of frames and the basename, and
-                    // is used to delete intermediate files (e.g. .ppm files) at the end.
-                    let frame_info: Option<(usize, &str)>;
-                    match exec::run_script(&cmds, tx) {
-                        Err(msg) => {
-                            println!("Error!\n{}", msg);
-                            frame_info = None;
-                        },
-                        Ok(opt_frame_info) => {
-                            frame_info = opt_frame_info;
-                            let elapsed = start.elapsed();
-                            println!("Time to generate frames in-memory: {} (includes some time saving images)", display_duration(elapsed));
-                        }
-                    }
-                    // Wait for worker threads to finish saving images
-                    handle.join();
-
-                    let elapsed = start.elapsed();
-                    println!("Elapsed time, after generating frames and converting to PNGs: {}", display_duration(elapsed));
-
-                    // If (multiple) frames were successfully generated, make a GIF and delete the rubbish
-                    if let Some((frames, basename)) = frame_info {
-                        ppm::convert_gif(frames, basename);
-                        ppm::clean_up_anim_ppms(frames, basename);
-                    }
-
-                    let elapsed_after_cleanup = start.elapsed();
-                    println!("Elapsed time, after cleaning up and converting to GIF: {}", display_duration(elapsed_after_cleanup));
-                },
-                Err(e) => {
-                    panic!("Error reading text in ./script: {}", e);
-                }
+        Ok(file) => {
+            let result = file.read_to_string(&mut s);
+            if let Err(e) = result {
+                println!("Error reading file 'script': {}", e);
+                return;
             }
         }
+    };
+
+    // `tx` is sent to run_script, which generates frames (on the heap) and
+    // sends them to `tx`. `rx` is given to spawn_saver which duplicates it
+    // into several worker threads which save frames to a file.
+    let (tx, rx) = channel();
+    let worker_pool_handle = ppm::spawn_saver(rx);
+    println!("Using {} worker threads to save and convert frames", NUM_WORKERS);
+
+    // Frames and gifs will be saved to anim directory
+    ppm::mkdirp("anim");
+
+    // Measure time from before we parse the script
+    let start = Instant::now();
+
+    // Parse script and exit on syntax error
+    let cmds = match parse::parse_script(&s) {
+        Ok(cmds) => cmds,
+        Err(parse_error) => {
+            println!("\n{}", parse_error);
+            return;
+        }
+    };
+
+    // If the script generates multiples frames, frame_info, is a pair of the number of frames and
+    // the basename, and is used to delete intermediate files (e.g. .ppm files) at the end.
+    let frame_info: Option<(usize, &str)>;
+    match exec::run_script(&cmds, tx) {
+        Err(msg) => {
+            frame_info = None;
+            println!("Error!\n{}", msg);
+        },
+        Ok(opt_frame_info) => {
+            frame_info = opt_frame_info;
+            let elapsed = start.elapsed();
+            println!("Time to generate frames in-memory: {} (includes some time saving images)", display_duration(elapsed));
+        }
     }
+
+    // Wait for worker threads to finish saving images
+    worker_pool_handle.join();
+
+    let elapsed = start.elapsed();
+    println!("Elapsed time, after generating frames and converting to PNGs: {}", display_duration(elapsed));
+
+    // If (multiple) frames were successfully generated, make a GIF and delete the rubbish
+    if let Some((frames, basename)) = frame_info {
+        ppm::convert_gif(frames, basename);
+        ppm::clean_up_anim_ppms(frames, basename);
+    }
+
+    let elapsed_after_cleanup = start.elapsed();
+    println!("Elapsed time, after cleaning up and converting to GIF: {}", display_duration(elapsed_after_cleanup));
 }
 
 fn display_duration(elapsed: std::time::Duration) -> String {
