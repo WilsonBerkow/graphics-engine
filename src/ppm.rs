fcd5cca2f1ad376d4f8ec2f0e222c9b9ca9f2cc2
Wilson Berkow <wilsonberkow@gmail.com>
2017-06-19 00:05:09 -0400

    Return io::Result in ppm.rs fns (instead of ignoring)

--- src/ppm.rs
+++ src/ppm.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::path::Path;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufWriter;
 use std::process::Command;
@@ -12,7 +13,7 @@ use worker::WorkerPool;
 use render::Screen;
 use consts::*;
 
-pub fn save_ppm(image: &Screen, filename: &str) {
+pub fn save_ppm(image: &Screen, filename: &str) -> io::Result<()> {
     let path = Path::new(filename);
     let path_display = path.display(); // For safe string formatting
     let mut file = match File::create(&path) {
@@ -23,16 +24,16 @@ pub fn save_ppm(image: &Screen, filename: &str) {
         }
         Ok(file) => file,
     };
-    write_image(&mut file, &image);
+    write_image(&mut file, &image)
 }
 
 pub fn spawn_saver(rx: Receiver<(String, Screen)>) -> WorkerPool {
     WorkerPool::new(rx, NUM_WORKERS)
 }
 
-pub fn save_png(image: &Screen, filename: &str) {
+pub fn save_png(image: &Screen, filename: &str) -> io::Result<()> {
     let tmp_name = format!("{}.ppm", filename);
-    save_ppm(image, &tmp_name);
+    save_ppm(image, &tmp_name)?;
     let start = Instant::now();
     let status = Command::new("convert")
         .arg(&tmp_name)
@@ -45,6 +46,7 @@ pub fn save_png(image: &Screen, filename: &str) {
     if !status.success() {
         println!("Execution of `convert {} {}` failed with status: {}", &tmp_name, filename, status);
     }
+    Ok(())
 }
 
 pub fn mkdirp(name: &str) {
@@ -113,8 +115,8 @@ pub fn display_file(filename: &str) {
 
 // Also do imagemagick converts in a smarter way (i.e. not every single time)
 
-pub fn display_image(image: &Screen) {
-    save_png(image, ".temp.png");
+pub fn display_image(image: &Screen) -> io::Result<()> {
+    save_png(image, ".temp.png")?;
     let status0 = Command::new("display")
         .arg(".temp.png")
         .status().ok().unwrap();
@@ -123,17 +125,21 @@ pub fn display_image(image: &Screen) {
         .arg(".temp.png")
         .status().ok().unwrap();
     println!("Execution of `rm .temp.ppm` exited with status: {}", status1);
+    Ok(())
 }
 
-pub fn write_image(file: &mut File, image: &Screen) {
+pub fn write_image(file: &mut File, image: &Screen) -> io::Result<()> {
     let start = Instant::now();
     let mut bufwriter = BufWriter::new(&*file);
     // P6 identifies the version of PPM in which colors are represented
     // in binary; as our max color value is 255, each RGB color is 3 bytes
-    bufwriter.write_fmt(format_args!("P6\n{} {} 255\n", WIDTH, HEIGHT));
-    bufwriter.write(image.as_bytes());
+    bufwriter.write_fmt(format_args!("P6\n{} {} 255\n", WIDTH, HEIGHT))?;
+    if let Err(e) = bufwriter.write(image.as_bytes()) {
+        return Err(e);
+    }
     if DEBUG {
         let elapsed = start.elapsed();
         println!("Saving took: {}ms {}ns", elapsed.as_secs() * 1000 + elapsed.subsec_nanos() as u64 / 1000000, elapsed.subsec_nanos() as u64 % 1000000);
     }
+    Ok(())
 }
